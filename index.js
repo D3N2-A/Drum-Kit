dced191dd467e2c49f2bf736c3c62b4f731c055d
D3N2-A <anmolsharma999937@gmail.com>
2022-05-29 15:08:29 +0530

    Event listener

---
+++ index.js
@@ -0,0 +1,7 @@
+for (var i = 0; i < document.getElementsByClassName("drum").length; i++) {
+  document
+    .getElementsByClassName("drum")
+    [i].addEventListener("click", function () {
+      alert("I got clicked!");
+    });
+}
